45e9790f806759acfe75626ec86afc25add0a64e
Shiruuuuuuu <92151393+Shiruuuuuuu@users.noreply.github.com>
2022-03-16 18:50:11 +0700

    Add project files.

---
+++ Lab3_TruongHoaiThinh/Controllers/GioHangController.cs
@@ -0,0 +1,131 @@
+﻿using Lab3_TruongHoaiThinh.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab3_TruongHoaiThinh.Controllers
+{
+    public class GioHangController : Controller
+    {
+        // GET: GioHang
+
+        MyDataDataContext data = new MyDataDataContext();
+        public List<Giohang> Laygiohang()
+        {
+            List<Giohang> listGiohang = Session["Giohang"] as List<Giohang>;
+            if (listGiohang == null)
+            {
+                listGiohang = new List<Giohang>();
+                Session["Giohang"] = listGiohang;
+            }
+            return listGiohang;
+        }
+        public ActionResult ThemGioHang(int id, string strURL)
+        {
+            List<Giohang> listGiohang = Laygiohang();
+            Giohang sanpham = listGiohang.Find(n => n.masach == id);
+            if (sanpham == null)
+            {
+                sanpham = new Giohang(id);
+                listGiohang.Add(sanpham);
+                return Redirect(strURL);
+            }
+            else
+            {
+                sanpham.iSoluong++;
+                return Redirect(strURL);
+            }
+        }
+
+        private int TongSoLuong()
+        {
+            int tsl = 0;
+            List<Giohang> listGiohang = Session["GioHang"] as List<Giohang>;
+            if (listGiohang != null)
+            {
+                tsl = listGiohang.Sum(n => n.iSoluong);
+            }
+            return tsl;
+        }
+
+        private int TongSoLuongSanPham()
+        {
+            int tsl = 0;
+            List<Giohang> listGiohang = Session["GioHang"] as List<Giohang>;
+            if (listGiohang != null)
+            {
+                tsl = listGiohang.Count;
+            }
+            return tsl;
+        }
+
+        private double TongTien()
+        {
+            double tt = 0;
+            List<Giohang> listGiohang = Session["GioHang"] as List<Giohang>;
+            if (listGiohang != null)
+            {
+                tt = listGiohang.Sum(n => n.dThanhtien);
+            }
+            return tt;
+        }
+
+        public ActionResult GioHang()
+        {
+            List<Giohang> listGiohang = Laygiohang();
+            ViewBag.Tongsoluong = TongSoLuong();
+            ViewBag.Tongtien = TongTien();
+            ViewBag.Tongsoluongsanpham = TongSoLuongSanPham();
+            return View(listGiohang);
+        }
+
+        public ActionResult GioHangPartial()
+        {
+            ViewBag.Tongsoluong = TongSoLuong();
+            ViewBag.Tongtien = TongTien();
+            ViewBag.Tongsoluongsanpham = TongSoLuongSanPham();
+            return PartialView();
+        }
+
+        public ActionResult XoaGiohang(int id)
+        {
+            List<Giohang> listGiohang = Laygiohang();
+            Giohang sanpham = listGiohang.SingleOrDefault(n => n.masach == id);
+            if(sanpham != null)
+            {
+                listGiohang.RemoveAll(n => n.masach == id);
+                return RedirectToAction("GioHang");
+            }
+            return RedirectToAction("GioHang");
+        }
+
+        public ActionResult CapnhatGiohang(int id, FormCollection collection)
+        {
+            List<Giohang> listGiohang = Laygiohang();
+            Giohang gioHang = listGiohang.SingleOrDefault(n => n.masach == id);
+            if (gioHang != null)
+            {
+                Sach sach = data.Saches.FirstOrDefault(m => m.masach == id);
+
+                int soluong = int.Parse(collection["txtSolg"].ToString());
+
+                if(soluong > sach.soluongton)
+                {
+                    return RedirectToAction("GioHang");
+                }
+                gioHang.iSoluong = soluong;
+            }
+            return RedirectToAction("GioHang");
+        }
+
+        public ActionResult XoaTatCaGioHang()
+        {
+            List<Giohang> listGioHang = Laygiohang();
+            listGioHang.Clear();
+            return RedirectToAction("GioHang");
+        }
+
+    }
+}
